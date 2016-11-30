4e002a5b3fd972561d56d2582f656b1de8f07b0c
justayar - Can Emre AYAR <cayar@ku.edu.tr>
2016-11-30 14:44:10 +0200

    Delete ocurrences project implementation

---
+++ DeleteOcurrnecesMoreThanNTimes/DeleteOcurrnecesMoreThanNTimes/Kata.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteOcurrnecesMoreThanNTimes
+{
+    class Kata
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+
+
+            int[] result_arr = new int[arr.Count()];
+
+
+            for (int i = 0; i < arr.Count(); i++)
+            {
+
+
+                int counter = 0;
+
+
+                int arr_nextposition = 0;
+
+
+                for (int j = 0; j < result_arr.Count(); j++)
+                {
+
+
+                    if (result_arr[j] == 0)
+                    {
+
+
+                        arr_nextposition = j;
+
+
+                        break;
+
+                    }
+
+
+
+                    if (result_arr[j] == arr[i])
+                    {
+
+                        counter++;
+
+                    }
+
+
+
+                }
+
+                if (counter < x)
+                {
+
+
+                    result_arr[arr_nextposition] = arr[i];
+
+                }
+
+
+            }
+
+
+            result_arr = result_arr.Where(val => val != 0).ToArray();
+
+
+            return result_arr;
+
+
+        }
+    }
+}
