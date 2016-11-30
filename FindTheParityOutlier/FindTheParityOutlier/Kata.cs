5aec8e44b30aebd68a53d58f4d56b69d45df8eb1
justayar - Can Emre AYAR <cayar@ku.edu.tr>
2016-11-30 14:46:41 +0200

    find the parity outlier project implementation

---
+++ FindTheParityOutlier/FindTheParityOutlier/Kata.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheParityOutlier
+{
+    class Kata
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Find(int[] integers)
+        {
+
+
+            if (integers[0] % 2 == 1 && integers[1] % 2 == 1)
+            {
+
+
+                for (int i = 2; i < integers.Count(); i++)
+                {
+
+
+
+                    if (integers[i] % 2 == 0)
+                    {
+
+
+                        return integers[i];
+
+
+                    }
+
+
+
+                }
+
+
+
+            }
+            else if (integers[0] % 2 == 0 && integers[1] % 2 == 0)
+            {
+
+                for (int i = 2; i < integers.Count(); i++)
+                {
+
+
+
+                    if (integers[i] % 2 == 1)
+                    {
+
+
+                        return integers[i];
+
+
+                    }
+
+
+
+                }
+
+
+            }
+            else {
+
+                if (integers[2] % 2 == integers[0] % 2)
+                {
+
+                    return integers[1];
+                }
+                else {
+
+
+                    return integers[0];
+
+                }
+
+
+            }
+
+
+            return -1;
+        }
+    }
+}
