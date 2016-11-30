b83f6375cface2d2ddcb58e72e04f67b02c808d7
justayar - Can Emre AYAR <cayar@ku.edu.tr>
2016-11-30 14:48:40 +0200

    get the middle character kata implementation

---
+++ GetTheMiddleCharacter/GetTheMiddleCharacter/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTheMiddleCharacter
+{
+    class Kata
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string GetMiddle(string s)
+        {
+
+            if (s.Length % 2 == 1)
+            {
+
+
+                return s[s.Length / 2].ToString();
+
+            }
+            else {
+
+                return s[s.Length / 2 - 1].ToString() + s[s.Length / 2].ToString();
+
+            }
+        }
+    }
+}
