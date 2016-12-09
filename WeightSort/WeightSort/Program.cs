4afaa36f7c48dfcd99be0e02be48686c6e8878d8
justayar - Can Emre AYAR <cayar@ku.edu.tr>
2016-12-09 16:27:38 +0200

    Weight sort kata implementation

---
+++ WeightSort/WeightSort/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(orderWeight("103 123 4444 99 2000"));
+
+            Console.ReadLine();
+
+        }
+
+        public static string orderWeight(string strng)
+        {
+
+            
+
+            if (strng == "") return "";
+
+
+
+            var listOfWeights = new List<KeyValuePair<string, int>>();
+
+
+            var weights = strng.Split(' ');
+
+            
+
+            foreach (string weight in weights)
+            {
+                int sum = 0;
+
+                foreach (char c in weight)
+                {
+                    sum += ((int)Char.GetNumericValue(c));
+
+                }
+
+                listOfWeights.Add(new KeyValuePair<string, int>(weight, sum));
+
+            }
+
+            listOfWeights = listOfWeights.OrderBy(x => x.Value).ThenBy(x => x.Key).ToList();
+
+           
+
+
+            strng = "";
+
+            foreach(KeyValuePair<string,int> str in listOfWeights)
+            {
+                strng += str.Key + " ";
+
+            }
+
+            return strng.Trim();
+       }
+    }
+}
